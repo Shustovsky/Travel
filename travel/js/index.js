adcc01ff688f1d32bea438697c57a55192b7bc70
A. Shustovsky <a.shustovsky@gmail.com>
2022-07-11 12:37:31 +0400

    feat: add js rules for hamburger

--- travel/js/index.js
+++ travel/js/index.js
@@ -1,21 +1,96 @@
-console.log('Вёрстка валидная +10 \nВёрстка семантическая +20 \nВ коде странице присутствуют следующие элементы (указано минимальное количество, может быть больше): \n<header>, <main>, <footer> +3 \nчетыре элемента <section> (по количеству секций) +3 \nтолько один заголовок <h1> +3 \nтри заголовка <h2> (количество секций минус одна, у которой заголовок <h1>) +3 \nодин элемент <nav> (панель навигации) +3 \nдва списка ul > li > a (панель навигации, ссылки на соцсети) +3 \nчетыре кнопки <button> +2 \nВёрстка соответствует макету +48 \nблок <header> +6 \nсекция preview +9 \nсекция steps +9 \nсекция destinations +9 \nсекция stories +9 \nблок <footer> +6 \nТребования к css + 12 \nдля построения сетки используются флексы или гриды +2  \nпри уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону +2 \nфоновый цвет тянется на всю ширину страницы +2 \nиконки добавлены в формате .svg. SVG может быть добавлен любым способом. Обращаем внимание на формат, а не на способ добавления +2 \nизображения добавлены в формате .jpg +2 \nесть favicon +2 \nИнтерактивность, реализуемая через css +20 \nплавная прокрутка по якорям +5 \nиконки соцсетей в футере при нажатии на них ведут на гитхаб автора проекта и на страницу курса (допускается доабвление своих вариантов иконок github или RSSchool) https://rs.school/js-stage0/ +5 \nинтерактивность включает в себя не только изменение внешнего вида курсора, например, при помощи свойства cursor: pointer, но и другие визуальные эффекты, например, изменение цвета фона или цвета шрифта. Если в макете указаны стили при наведении и клике, для элемента указываем эти стили. Если в макете стили не указаны, реализуете их по своему усмотрению, руководствуясь общим стилем макета +5 \nобязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +5\n');
+console.log(`Вёрстка соответствует макету. Ширина экрана 390px +48
+блок <header> +6
+секция preview +9
+секция steps +9
+секция destinations +9
+секция stories +9
+блок <footer> +6
+Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15
+нет полосы прокрутки при ширине страницы от 1440рх до 390px +7
+нет полосы прокрутки при ширине страницы от 390px до 320рх +8
+На ширине экрана 390рх и меньше реализовано адаптивное меню +22
+при ширине страницы 390рх панель навигации скрывается, появляется бургер-иконка +2
+при нажатии на бургер-иконку плавно появляется адаптивное меню +4
+адаптивное меню соответствует макету +4
+при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4
+ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4 (все кроме Account, она пока что просто закрывает меню)
+при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4
+`);
 
 
 let navBurger = document.querySelector('.nav__burger');
 let burgerMenu = document.querySelector('.burger__menu');
-let burgerClose = document.querySelector('.burger__menu_close')
-let burgerList = document.querySelector('.burger__menu_list')
 
-navBurger.addEventListener('click', function() {
+const toggleMenu = () => {
     burgerMenu.classList.toggle('burger__menu-active');
+}
+
+navBurger.addEventListener('click', e => {
+    e.stopPropagation();
+
+    toggleMenu();
+});
+
+document.addEventListener('click', e => {
+    let target = e.target;
+    let its_menu = target == burgerMenu || burgerMenu.contains(target);
+    let its_burger = target == navBurger;
+    let menu_is_active = burgerMenu.classList.toggle('burger__menu-active');
+
+    if (!its_menu && !its_burger && menu_is_active) {
+        toggleMenu();
+    }
 })
-burgerClose.addEventListener('click', function() {
-    burgerMenu.classList.toggle('burger__menu-active');
-})
-burgerList.addEventListener('click', function() {
-        burgerMenu.classList.toggle('burger__menu-active');
-    })
-    //////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+// let navBurger = document.querySelector('.nav__burger');
+// let burgerMenu = document.querySelector('.burger__menu');
+// let burgerClose = document.querySelector('.burger__menu_close')
+// let burgerList = document.querySelector('.burger__menu_list')
+
+// navBurger.addEventListener('click', function() {
+//     burgerMenu.classList.toggle('burger__menu-active');
+// });
+// burgerClose.addEventListener('click', function() {
+//     burgerMenu.classList.toggle('burger__menu-active');
+// });
+// burgerList.addEventListener('click', function() {
+//     burgerMenu.classList.toggle('burger__menu-active');
+// });
+// burgerMenu.addEventListener('click', function() {
+//     if ('burger__menu-active' === true) {
+//         burgerMenu.classList.toggle('burger__menu-active');
+
+//     }
+// });
+
+
+//////////////////////////////////////////////////////////////
+
+// let buttonPopup = document.querySelector('.btn_popup');
+// let popup = document.querySelector('.popup');
+
+// const toggleMenuPopup = () => {
+//     popup.classList.toggle('popup-active');
+// }
+
+// buttonPopup.addEventListener('click', e => {
+//     e.stopPropagation();
+
+//     toggleMenuPopup();
+// });
+
+// document.addEventListener('click', e => {
+//     let target = e.target;
+//     let its_menu = target == popup || popup.contains(target);
+//     let its_burger = target == buttonPopup;
+//     let menu_is_active = popup.classList.toggle('popup-active');
+
+//     if (!its_menu && !its_burger && menu_is_active) {
+//         toggleMenuPopup();
+//     }
+// })
+
 
 let buttonPopup = document.querySelector('.btn_popup');
 let popup = document.querySelector('.popup');
