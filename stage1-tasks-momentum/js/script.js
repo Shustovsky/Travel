eb06d2871f50dfe6fa303c5caf0c25d9be41401b
A. Shustovsky <a.shustovsky@gmail.com>
2022-08-04 11:08:19 +0400

    feat: add welcome function 😊

--- stage1-tasks-momentum/js/script.js
+++ stage1-tasks-momentum/js/script.js
@@ -1,6 +1,8 @@
 // time & calendar
 const date = document.querySelector('.date');
 const time = document.querySelector('.time');
+const greeting = document.querySelector('.greeting');
+const input = document.querySelector('.name');
 
 showDate = () => {
     const fullDate = new Date();
@@ -9,11 +11,45 @@ showDate = () => {
     date.textContent = currentDate;
 }
 
+showGreeting = () => {
+    getTimeOfDay = () => {
+        const fullDate = new Date();
+        const hours = fullDate.getHours();
+
+        if (hours <= 6) {
+            return 'night';
+        } else if (hours <= 12) {
+            return 'morning';
+        } else if (hours <= 18) {
+            return 'day';
+        } else if (hours <= 24) {
+            return 'evening';
+        }
+    };
+    const timeOfDay = getTimeOfDay();
+    const greetingText = `Good ${timeOfDay}`;
+    greeting.textContent = greetingText;
+}
+
 showTime = () => {
     const fullDate = new Date();
     const currentTime = fullDate.toLocaleTimeString();
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
     showDate();
+    showGreeting();
 }
 showTime();
+
+
+function setLocalStorage() {
+    localStorage.setItem('name', input.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        input.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage)
